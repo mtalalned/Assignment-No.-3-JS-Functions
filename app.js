d4821450ba46ecd8acab52d1d92997d92947cadd
mtalalned <muhammadtalalned@gmail.com>
2024-07-17 12:54:41 -0700

    7 questions completed

---
+++ app.js
@@ -0,0 +1,64 @@
+// Question No. 1
+// function printHello() {
+//     console.log ('Hello User')
+// }
+// printHello()
+
+// Question No. 2
+// var p = document.querySelector ('#p')
+// function date_time(){
+//     p.innerHTML = Date();
+// }
+
+// Question No.3
+// function greetUser (firstname , lastname) {
+//     alert ('Welcome Mr. / Mrs.'+" "+firstname+" "+lastname+" "+"to our webpage")
+// }
+// greetUser ('Muhammad', 'Talal')
+
+// Question No. 4
+// function addNumbers () {
+//     var num1 = +prompt('Enter first number')
+//     var num2 = +prompt('Enter second number')
+//     return num1+ num2
+// }
+
+// Question No. 5
+// function calculator (num1,num2,operator) {
+//     if (operator === "+") {
+//         return num1+num2
+//     } else if(operator === "-") {
+//         return num1 - num2
+//     } else if (operator === "*"){
+//         return num1*num2
+//     }else if (operator === "/") {
+//         return num1/num2
+//     }else {
+//         return 'Enter operator'
+//     }
+// }
+
+// Question No. 6
+// function square(number) {
+//     return number*number
+// }
+
+// Question No. 7
+// var num;
+// var product;
+// function factorial (number) {
+//     if (number > 1 && product == undefined) {
+//         num = number - 1
+//         product = number * num
+//         number = num 
+//         factorial (number)
+//     }else if (number > 1){
+//         num = number - 1
+//         product = product * num
+//         number = num 
+//         factorial (number)
+//     }else {
+//         console.log ('The factorial of the number is', product)
+//     }
+//     return product
+// }
